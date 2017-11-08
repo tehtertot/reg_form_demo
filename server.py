2118f4bf7e415919ff91cf338cb78002b0cfe13f
Noelle <noelle.teh@gmail.com>
2017-11-07 17:18:26 -0800

    added db

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, flash, session, redirect
 import re
+from mysqlconnection import MySQLConnector
 
 app = Flask(__name__)
 
 app.secret_key = "secret secret secret"
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+mysql = MySQLConnector(app, "wall_demo")
 
 @app.route('/')
 def index():
@@ -47,8 +49,43 @@ def register():
         is_valid = False
 
     if is_valid:
-        flash("Thanks for submitting your information.")
+        add_user = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (:fn, :ln, :em, :pw, NOW(), NOW())"
+        user_data = { 'fn': request.form["fname"],
+                      'ln': request.form["lname"],
+                      'em': request.form["email"],
+                      'pw': request.form["pw"]}
+        user_id = mysql.query_db(add_user, user_data)
+        #set user in session
+        session["name"] = request.form["fname"]
+        session["user_id"] = user_id
+        return redirect('/wall')
         
     return redirect('/')
+
+@app.route('/login', methods=["POST"])
+def login():
+    #is there a user with that email in my db?
+    find_user_q = "SELECT * FROM users WHERE email = :email"
+    data = { 'email': request.form["email"]}
+    found_user = mysql.query_db(find_user_q, data)
+
+    #no user with that email
+    if len(found_user) == 0:
+        flash("No user registered with that email")
+    else:
+        #if so, does the password they entered match what is in the db?
+        if found_user[0]["password"] != request.form["pw"]:
+            flash("Password is incorrect")
+        else:
+            #set user in session
+            session["name"] = found_user[0]["first_name"]
+            session["user_id"] = found_user[0]["user_id"]
+            return redirect('/wall')
+    
+    return redirect('/')
+
+@app.route('/wall')
+def show_wall():
+    return render_template('wall.html')
 
 app.run(debug=True)
